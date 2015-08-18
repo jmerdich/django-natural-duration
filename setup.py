fe87ce2d340b14acb1013acac7174924a5c716b1
Jake Merdich <jake@merdich.com>
2015-08-18 04:00:42 -0700

    Missed a comma

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     install_requires=[
         'Django>=1.8,<1.9'
     ],
-    keywords = ['forms', 'timedelta', 'humanize']
+    keywords = ['forms', 'timedelta', 'humanize'],
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
